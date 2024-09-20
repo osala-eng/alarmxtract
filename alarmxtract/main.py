9fadcd3478fc2a702a615a661ae8f7fe5f80fa1a
Jashon Osala <jashonosala@gmail.com>
2024-09-20 15:36:11 +0300

    chore: Format files.

--- alarmxtract/main.py
+++ alarmxtract/main.py
@@ -1,4 +1,16 @@
-import PySimpleGUI as sg
 import os
 from datetime import datetime
 
+import PySimpleGUI as sg
+
+
+def extract_alarm_logs(app_folder: str, start_date: datetime, end_date: datetime, output_folder: str):
+    """ Read amd Extract contents of .alh files to excel. """
+
+    alarm_dir = os.path.join(app_folder, "alarm")
+
+    if not os.path.exists(alarm_dir):
+        sg.popup("Error", "Alarm directory not found")
+        return
+
+    sg.popup("Success", f"Alarm logs from {start_date} to {end_date} have been extracted and saved to {output_folder}")
